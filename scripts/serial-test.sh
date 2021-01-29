eceb2e3bae997d8b249dceedaea1851f18ee846c
Matt Witherspoon <32485495+spoonincode@users.noreply.github.com>
2021-01-29 16:39:25 -0500

    wip

--- scripts/serial-test.sh
+++ scripts/serial-test.sh
@@ -8,6 +8,8 @@ if [[ "$(uname)" == 'Linux' ]]; then
     . /etc/os-release
     if [[ "$ID" == 'centos' ]]; then
         [[ -f /opt/rh/rh-python36/enable ]] && source /opt/rh/rh-python36/enable
+        #rh-postgresql12 only installed on unpinned centos7 environments
+        [[ -f /opt/rh/rh-postgresql12/enable ]] && source /opt/rh/rh-postgresql12/enable
     fi
     cd "$GIT_ROOT"
 fi
